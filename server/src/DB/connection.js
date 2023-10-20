436fa938a877094028ca3cff9d8a4e9a1f457f20
webGhoul <84246173+web-ghoul@users.noreply.github.com>
2023-10-20 07:25:50 +0300

    JKeep Changes

--- server/src/DB/connection.js
+++ server/src/DB/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
-console.log(process.env.DB_URL);
+
 const DB_Connect = mongoose.connect(process.env.DB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
